10d6b4bdf46d05678973ae7584fcb2f9ed55e555
Maya Kim <mayagy.kim@gmail.com>
2024-10-11 10:18:29 +0200

    added style libraries

---
+++ ecom-project/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+    "./node_modules/flowbite/**/*.js" // set up the path to the flowbite package
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin') // add the flowbite plugin
+  ],
+}
