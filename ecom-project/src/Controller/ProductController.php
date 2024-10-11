31ec50bfad1627722bf80f8b106655b01fc42b1d
Maya Kim <mayagy.kim@gmail.com>
2024-10-11 14:26:48 +0200

    created product creation form and created 3 products

---
+++ ecom-project/src/Controller/ProductController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Products;
+use App\Form\ProductType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProductController extends AbstractController
+{
+  #[Route('/product', name: 'app_product')]
+  public function index(Request $request, EntityManagerInterface $entity): Response
+  {
+      $product = new Products();
+      $form = $this->createForm(ProductType::class, $product);
+      $form->handleRequest($request);
+
+      if ($form->isSubmitted() && $form->isValid()){
+        $entity->persist($product);
+        $entity->flush();
+        $this->addFlash("message", "Produit ajouté avec succès");
+        return $this->redirectToRoute("app_product");
+      }
+
+      return $this->render('product/index.html.twig', [
+          'form' => $form->createView(),
+      ]);
+    }
+}
