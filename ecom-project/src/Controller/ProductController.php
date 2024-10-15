040a37aa29c138d66b83e8fac21b1eb14f30b94a
Maya Kim <mayagy.kim@gmail.com>
2024-10-15 14:54:44 +0200

    created modification and delete of products/categories feature

--- ecom-project/src/Controller/ProductController.php
+++ ecom-project/src/Controller/ProductController.php
@@ -44,6 +44,54 @@ class ProductController extends AbstractController
       ]);
     }
 
+    #[Route('/product/modif/{id}', name: 'app_productmodif')]
+      public function modif($id, Request $request, EntityManagerInterface $entity): Response
+      {
+          $product = $entity->getRepository(Products::class)->find($id);
+          $form = $this->createForm(ProductType::class, $product);
+          $form->handleRequest($request);
+
+          if ($form->isSubmitted() && $form->isValid()){
+            $entity->persist($product);
+            $entity->flush();
+            $this->addFlash("message", "Produit modifié avec succès");
+            return $this->redirectToRoute("app_product");
+          }
+          // Formulaire pour catégories
+          $category = $entity->getRepository(Categories::class)->find($id);
+          $formCategorie = $this->createForm(CategoriesType::class, $category);
+          $formCategorie->handleRequest($request);
+          if($formCategorie->isSubmitted() && $formCategorie->isValid()){
+            $entity->persist($category);
+            $entity->flush();
+            $this->addFlash("message", "Catégorie modifié avec success");
+            return $this->redirectToRoute('app_productadd');
+          }
+
+          return $this->render('product/add.html.twig', [
+              'form' => $form->createView(),
+              'formCategorie' =>$formCategorie,
+          ]);
+        }
+
+        #[Route('/product/suppProduit/{id}', name: 'app_productsupp')]
+        public function suppPro($id, Request $request, EntityManagerInterface $entity): Response
+        {
+            $product = $entity->getRepository(Products::class)->find($id);
+            $entity->remove($product);
+            $entity->flush();
+            return $this->redirectToRoute('app_product');
+          }
+
+        #[Route('/product/suppCategories/{id}', name: 'app_productsuppCat')]
+        public function suppCat($id, Request $request, EntityManagerInterface $entity): Response
+        {
+            $category = $entity->getRepository(Categories::class)->find($id);
+            $entity->remove($category);
+            $entity->flush();
+            return $this->redirectToRoute('app_product');
+          }
+
 
   #[Route('/product', name: 'app_product')]
   public function index(Request $request, EntityManagerInterface $entity): Response
