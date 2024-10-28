9c40a7b802de2d0eb2f181d17aa39cce73fa30e5
Maya Kim <mayagy.kim@gmail.com>
2024-10-28 17:12:44 +0100

    created iteration for displaying menus

--- yummy-notes-by-maya/src/Controller/MainController.php
+++ yummy-notes-by-maya/src/Controller/MainController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Menus;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +12,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function home(): Response
+    public function home(EntityManagerInterface $entity): Response
     {
+      $menus = $entity->getRepository(Menus::class)->findAll();
+      // $topMenu :
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'menus' => $menus,
         ]);
     }
 
