bf3c2f20ebfeded2af03f9d3cff8d7c3d65b8237
Maya Kim <mayagy.kim@gmail.com>
2024-11-19 14:17:41 +0100

    created panier functions (add, index)

--- yummy-notes-by-maya/src/Controller/PanierController.php
+++ yummy-notes-by-maya/src/Controller/PanierController.php
@@ -2,17 +2,52 @@
 
 namespace App\Controller;
 
+use App\Repository\MenusRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PanierController extends AbstractController
 {
     #[Route('/panier', name: 'app_panier')]
-    public function index(): Response
+    public function index(SessionInterface $session, MenusRepository $menu): Response
     {
-        return $this->render('panier/index.html.twig', [
-            'controller_name' => 'PanierController',
-        ]);
+      $panier = $session->get('panier', []);
+
+      $data = [];
+      $total = 0;
+
+      foreach($panier as $key => $quantity){
+        $produit = $menu->find($key);
+
+        $data[] = [
+          "menu"=>$produit,
+          "quantity"=>$quantity
+        ];
+        $total += $produit->getTime() * $quantity;
+      }
+
+      // dd($total);
+      return $this ->render('panier/index.html.twig', [
+        'data'=>$data,
+        'total'=>$total,
+      ]);
+    }
+
+    #[Route('/panier/add/{id}', name: 'app_add_panier')]
+    public function add($id, SessionInterface $session): Response
+    {
+      $panier = $session->get('panier', []);
+
+      if(empty($panier[$id])) {
+        $panier[$id] = 1;
+      }else{
+        $panier[$id]++;
+      }
+
+      $session->set('panier', $panier);
+
+      return $this->redirectToRoute("app_panier");
     }
 }
